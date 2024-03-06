c896d5928fd20df385314561b7b426b605465830
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-06 23:45:10 +0530

    Throwing Not Found Errors For Individual Meals

--- src/app/meals/[mealSlug]/page.tsx
+++ src/app/meals/[mealSlug]/page.tsx
@@ -2,35 +2,36 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import { getMeal } from '../../../../lib/meals';
 import { type MealsType } from '../../../../lib/meals';
+import { notFound } from 'next/navigation';
 
 function MealDetailsPage({ params }: { params: { mealSlug: string } }) {
   const meal: MealsType | undefined = getMeal(params.mealSlug);
 
+  if (!meal) {
+    notFound();
+  }
+  // Either this or ensure that meal exist or if meal exist only then render its content
+
   let instructions: string | undefined;
-  if (meal?.instructions) {
-    instructions = meal?.instructions.replace(/\n/g, '<br>');
+  if (meal.instructions) {
+    instructions = meal.instructions.replace(/\n/g, '<br>');
   }
   return (
     <>
       <header className={styles.header}>
         <div className={styles.image}>
-          {meal && (
-            <Image
-              src={meal.image}
-              alt={meal.title}
-              fill
-            />
-          )}
+          <Image
+            src={meal.image}
+            alt={meal.title}
+            fill
+          />
         </div>
         <div className={styles.headerText}>
           {meal && <h1>{meal.title}</h1>}
           <p className={styles.creator}>
-            by{' '}
-            {meal && (
-              <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
-            )}
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={styles.summary}>{meal?.summary}</p>
+          <p className={styles.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
